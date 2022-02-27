74c019d06d81285ef02e3e64b0f5d818049e4238
SilRodrigo <rodrigo.sil91@gmail.com>
2022-02-27 14:31:28 -0300

    testes

--- js/youtubeApi.js
+++ js/youtubeApi.js
@@ -1,12 +1,14 @@
-var tag = document.createElement('script'),
-    player;
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        videoId: document.querySelector("[yt-link]").getAttribute('yt-link')
-    });
-}
+window.addEventListener('load', () => {
+    var tag = document.createElement('script'),
+        player;
+    
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            videoId: document.querySelector("[yt-link]").getAttribute('yt-link')
+        });
+    }    
+});
