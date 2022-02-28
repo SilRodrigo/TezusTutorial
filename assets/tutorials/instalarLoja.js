c2af6e2f5ee9226f862367c17746b6e235dffba1
SilRodrigo <rodrigo.sil91@gmail.com>
2022-02-27 23:57:49 -0300

    Finalizado stampagem do yt

--- assets/tutorials/instalarLoja.js
+++ assets/tutorials/instalarLoja.js
@@ -3,15 +3,16 @@ export default {
     sub_title: 'Siga os passos abaixo para a instalação:',
     yt_link: 'h5nwnsiN_Hw',
     steps: [
-        new step('<a>Pesquisa no git da Tezus o Docker Magento</a>', null, 'https://github.com/tezusecommerce/docker-magento'),
-        new step('Copia a URL', { 'url-copy': "https://github.com/tezusecommerce/docker-magento" }),
+        new step('<a>Pesquisa no git da Tezus o Docker Magento</a>',  { 'yt-timestamp': "0" }, 'https://github.com/tezusecommerce/docker-magento'),
+        new step('Copia a URL', { 'url-copy': "https://github.com/tezusecommerce/docker-magento",  'yt-timestamp': "14" }),
         new step('No terminal:', null, null, true),
-        new step('Acessa a pasta onde as Lojas ficarão salvas usando o comando "cd"', { 'yt-timestamp': "120" }),
-        new step('<pre>git clone "colar URL copiada" "NomeLoja"</pre>'),
-        new step('Acessa a pasta "NomeLoja"<pre>cd "NomeLoja"</pre><pre>sudo mkdir .vscode</pre><pre>sudo rm -r .git</pre>'),
-        new step('Baixe a versão para iniciar a instalação do docker<pre>bin/download "número da versão"</pre><pre>Insira suas chaves Magento quando solicitado</pre>'),
-        new step('Rode a instalação do docker<pre>bin/setup docker.tezus</pre>'),
-        new step('Se for preciso dados na loja, baixa o módulo SampleData<pre>bin/magento sampledata:deploy</pre>Para ver quais módulos estão ativos ou desabilitados<pre>module:status</pre><pre>bin/magento module:enable --all (ativa todos os módulos)</pre>Desabilita o módulo TwoFactorAuth<pre>bin/magento module:disable Magento_TwoFactorAuth</pre>')
+        new step('Acessa a pasta onde as Lojas ficarão salvas usando o comando "cd"', { 'yt-timestamp': "120", 'yt-timestamp': "21" }),
+        new step('<pre>git clone "colar URL copiada" "NomeLoja"</pre>',  { 'yt-timestamp': "28" }),
+        new step('Acessa a pasta "NomeLoja"<pre>cd "NomeLoja"</pre><pre>sudo mkdir .vscode</pre><pre>sudo rm -r .git</pre>',  { 'yt-timestamp': "45" }),
+        new step('Baixe a versão para iniciar a instalação do docker<pre>bin/download "número da versão"</pre>Insira suas chaves Magento quando solicitado',  { 'yt-timestamp': "67" }),
+        new step('Rode a instalação do docker<pre>bin/setup docker.tezus</pre>Informe as chaves Magento novamente no fim do setup',  { 'yt-timestamp': "122" }),
+        new step('Ao fim do setup desabilite o módulo Magento_TwoFactorAuth<pre>bin/magento module:disable Magento_TwoFactorAuth</pre><pre>bin/magento setup:di:compile</pre>',  { 'yt-timestamp': "178" }),
+        new step('Em seguida acesse a loja no navegador:<a>https://docker.tezus</a>',  { 'yt-timestamp': "214" }, 'https://docker.tezus'),
     ]
 }
 
